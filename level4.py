432b495ee200f2a62dfe27ef6f0eaf8296e3d6d4
Pooja0413 <poojanair412003@gmail.com>
2026-06-22 16:11:12 +0530

    master

---
+++ level4.py
@@ -0,0 +1,76 @@
+#1. Question : Get a Two-digit number from user and print the digit in “Ones”
+# position
+# Output : 
+# User Enters “78”
+# Answer - 8
+
+# def main():
+#  x = int(input("Enter Number: "))
+#  y = x%10
+#  print(f"Result = {y}")
+# if __name__ == "__main__":
+#  main()
+
+# Question : Get a Two digit number from user and print the digit in “Tens”
+# position
+# Output : 
+# User Enters “78”
+# Answer - 7
+
+# def main():
+#  x = int(input("Enter Number: "))
+#  y = x//10
+#  print(f"Result = {y}")
+# if __name__ == "__main__":
+#  main()
+
+# Question :Get a Three digit number from user and print the digit in “Ones”
+# position
+# Output : 
+# User Enters “738”
+# Answer - 8
+
+# def main():
+#  x = int(input("Enter Number: "))
+#  y = x%10
+#  print(f"Result = {y}")
+# if __name__ == "__main__":
+#  main()
+ 
+#  Get a Three digit number from user and print the digit in “Tens”
+# position
+# Output : 
+# User Enters “738”
+# Answer - 3
+
+# def main():
+#  x = int(input("Enter Number: "))
+#  y = int((x/10)%10)
+#  print(f"Result = {y}")
+# if __name__ == "__main__":
+#  main()
+
+# Question :Get a Three digit number from user and print the digit in 
+# “Hundreds” position
+# Output : 
+# User Enters “738”
+# Answer - 7
+
+# def main():
+#  x = int(input("Enter Number: "))
+#  y = x//100
+#  print(f"Result = {y}")
+# if __name__ == "__main__":
+#  main()
+
+# Question :Get a Two digit number from the user and print the reverse of it.
+# Output : 
+# User Enters “73”
+# Answer - 37
+
+def main():
+ x = int(input("Enter Number: "))
+ y = x//10
+ print(f"Result = {y}")
+if __name__ == "__main__":
+ main()
