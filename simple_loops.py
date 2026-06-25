e835e1ee48856ab4ed6ac1566640794e46399084
Pooja0413 <poojanair412003@gmail.com>
2026-06-25 09:31:35 +0530

    pyque

---
+++ simple_loops.py
@@ -0,0 +1,52 @@
+# Question : Write a loop program to print 1 to 5 on one by one.
+# Output : 
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# for i in range(1,6):
+#     print(i)
+
+# Question : Write a loop program to print 5 to 1 on one by one.
+# Output : 
+# 5
+# 4
+# 3
+# 2
+# 1
+
+# for i in range(5,0,-1):
+#     print(i)
+
+# Question : Write a loop program to print sum of 1 to 5.
+# Output : 
+# 15
+
+# sum=0
+# for i in range(1,6):
+#     sum+=i
+# print(sum)
+
+# Question : Write a loop program to print odd numbers 1 to 9.
+# Output : 
+# 1
+# 3
+# 5
+# 7
+# 9
+
+# for i in range(1,10):
+#     if(i%2!=0):
+#         print(i)
+
+
+# Question :Write a loop program to print sum of 5 to 1.
+# Output : 
+# 21
+
+# sum = 0
+# for i in range(5,0,-1):
+#     sum += i
+# print(sum)  
